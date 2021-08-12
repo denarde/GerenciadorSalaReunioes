3bd3facf715ebc89235e5d666192bee1e085b4a6
Luiz Denarde <luiz.denarde@senior.com.br>
2021-08-12 16:32:02 -0300

    Base do projeto

---
+++ src/main/java/br/com/reunioes/GerenciadorSalaReunioes/GerenciadorSalaReunioesApplication.java
@@ -0,0 +1,13 @@
+package br.com.reunioes.GerenciadorSalaReunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSalaReunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSalaReunioesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/reunioes/GerenciadorSalaReunioes/GerenciadorSalaReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reunioes.GerenciadorSalaReunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSalaReunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
